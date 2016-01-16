0688f196275bdf9095d54bcf29ce950b4164a998
venegator <m.venega94@gmail.com>
2016-01-16 13:08:24 +0100

    añadidos algunos estilos

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+	$( "#clickme" ).click(function() {
+		  $( "#ingresos" ).slideUp( "slow", function() {
+		    // Animation complete.
+		  });
+	});
+});
+
+// $(document).ready(function () {
+	
+//     $("#inicio-link").click(function(){
+// 		$("#inicio-link").addClass("active");		
+// 		$("#diseñadores-link").removeClass("active");
+// 		$("#modelos-link").removeClass("active");
+// 		$("#ciudad-link").removeClass("active");
+// 		$("#estadisticas-link").removeClass("active");
+// 		$("#galeria-link").removeClass("active");
+// 	});
+
+
+// 	$("#diseñadores-link").click(function(){
+// 		$("#diseñadores-link").addClass("active");
+// 		$("#inicio-link").removeClass("active");
+// 		$("#modelos-link").removeClass("active");
+// 		$("#ciudad-link").removeClass("active");
+// 		$("#estadisticas-link").removeClass("active");
+// 		$("#galeria-link").removeClass("active");
+// 	});
+
+// 	$("#modelos-link").click(function(){
+// 		$("#modelos-link").addClass("active");
+// 		$("#inicio-link").removeClass("active");		
+// 		$("#diseñadores-link").removeClass("active");
+// 		$("#ciudad-link").removeClass("active");
+// 		$("#estadisticas-link").removeClass("active");
+// 		$("#galeria-link").removeClass("active");
+		
+// 	});
+
+// 	$("#ciudad-link").click(function(){
+// 		$("#ciudad-link").addClass("active");
+// 		$("#inicio-link").removeClass("active");		
+// 		$("#diseñadores-link").removeClass("active");
+// 		$("#modelos-link").removeClass("active");
+// 		$("#estadisticas-link").removeClass("active");
+// 		$("#galeria-link").removeClass("active");
+// 	});
+
+
+// 	$("#estadisticas-link").click(function(){
+// 		$("#estadisticas-link").addClass("active");
+// 		$("#inicio-link").removeClass("active");		
+// 		$("#diseñadores-link").removeClass("active");
+// 		$("#modelos-link").removeClass("active");
+// 		$("#ciudad-link").removeClass("active");
+// 		$("#galeria-link").removeClass("active");
+// 	});
+
+
+// 	$("#galeria-link").click(function(){
+// 		$("#galeria-link").addClass("active");
+// 		$("#inicio-link").removeClass("active");		
+// 		$("#diseñadores-link").removeClass("active");
+// 		$("#modelos-link").removeClass("active");
+// 		$("#ciudad-link").removeClass("active");
+// 		$("#estadisticas-link").removeClass("active");
+// 	});
+// 	});
+
+// });
+
+
